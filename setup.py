11125c6a4f7a4ad94574f370cc583df94ff92fdc
untitledC20H12 <95670812+C20H12@users.noreply.github.com>
2023-02-27 23:26:20 -0700

    tried new file

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'inputHelp',
   packages = ['inputHelp'],
-  version = '1.3',
+  version = '1.4',
   license='MIT',
   description = 'Contains functions to validate input for command line programs',
   author = 'c20h12untitled',
   url = 'https://github.com/C20H12/inputHelpPackage',
-  download_url = 'https://github.com/C20H12/inputHelpPackage/archive/refs/tags/1.3.tar.gz',
+  download_url = 'https://github.com/C20H12/inputHelpPackage/archive/refs/tags/1.4.tar.gz',
   keywords = ['input', 'command line'],
   install_requires=[],
   classifiers=[
